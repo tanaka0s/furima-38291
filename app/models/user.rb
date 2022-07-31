04e71b9608579761026a3e96ce391c4f36496e40
tanaka0s <arima131409@gmail.com>
2022-07-31 19:06:58 +0900

    usersテーブルのバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :encrypted_password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/ }
+         validates :nickname,        presence: true
+         validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/}
+         validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/}
+         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+         validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+         validates :birth_date,      presence: true
 end
