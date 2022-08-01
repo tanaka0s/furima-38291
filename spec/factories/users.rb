4fb0cf7d1193ad3e5950484dd7ec3894874e4159
tanaka0s <arima131409@gmail.com>
2022-08-01 23:05:06 +0900

    userモデルの単体テスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.name}
+    email {Faker::Internet.free_email}
+    password {"1a" + Faker::Internet.password(min_length: 4, mix_case: false)}
+    password_confirmation {password}
+    last_name {"増田"}
+    first_name {"太郎"}
+    last_name_kana {"マスダ"}
+    first_name_kana {"タロウ"}
+    birth_date {Faker::Date.between(from: '1930-01-01', to: '2017-12-31')}
+  end
+end
