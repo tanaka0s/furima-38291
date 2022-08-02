ca917cc7d22165413fd5e0422a2e2e25b35d2a1a
tanaka0s <arima131409@gmail.com>
2022-08-02 20:00:34 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,23 @@
 class Item < ApplicationRecord
   belongs_to       :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price
+  end
+  with_options numericality: { other_than: 1, message: "can't be blank"}
+    validates :category_id 
+    validates :condition_id
+    validates :delivery_charge_id
+    validates :prefecture_id
+    validates :shipping_day_id
 end
