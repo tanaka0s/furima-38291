564f360fb905564485ff018b2df2545f45b895df
tanaka0s <arima131409@gmail.com>
2022-08-03 15:48:43 +0900

    itemモデルの単体テスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,10 +13,9 @@ class Item < ApplicationRecord
     validates :name
     validates :description
     validates :image
-    with_options format: { with: /\A[0-9]+\z/, message: 'is invalid.Input half-width characters' } do
-      validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is out of setting range'}
-    end
+    validates :price, numericality: { with: /\A[0-9]+\z/, message: 'is invalid.Input half-width characters' } 
   end
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is out of setting range'}
   with_options numericality: { other_than: 1, message: "can't be blank"} do
     validates :category_id 
     validates :condition_id
