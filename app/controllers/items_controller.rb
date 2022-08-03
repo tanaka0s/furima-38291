7aa0336f7762eb75659ae3eb6b0f172ca1db97ed
tanaka0s <arima131409@gmail.com>
2022-08-04 01:06:06 +0900

    エラーハンドリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_index, only: :edit
+
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -39,5 +41,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :delivery_charge_id, :prefecture_id,
                                  :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+      @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to action: :index 
+    end
   end
 end
