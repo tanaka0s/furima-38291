72d7b9a226ee3eb2e28b0b7cc5bb838c381f70a9
tanaka0s <arima131409@gmail.com>
2022-08-04 20:12:24 +0900

    購入機能エラーハンドリング

--- app/models/order_purchaser.rb
+++ app/models/order_purchaser.rb
@@ -5,13 +5,13 @@ class OrderPurchaser
   with_options presence: true do
     validates :user_id
     validates :item_id
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
+    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
     validates :city
     validates :house_number
     validates :phone_number,length: {minimum: 10, maximum: 11, message: "is too short"}
-    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
   end
-  validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Input only number"}
-  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Input only number"}
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
