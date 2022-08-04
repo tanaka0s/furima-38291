9e7610a9184bbe66e251dbaf2756d9f9a7fdc3d1
tanaka0s <arima131409@gmail.com>
2022-08-04 19:53:07 +0900

    購入機能のパラメーター設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,5 +5,19 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @order_purchaser = OrderPurchaser.new(order_purchaser_params)
+    if @order_purchaser.valid?
+      @order_purchaser.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_purchaser_params
+    params.require(:order_purchaser).permit(:post_code, :prefecture_id, :city, :house_number, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
--- app/models/order_purchaser.rb
+++ app/models/order_purchaser.rb
@@ -1,6 +1,6 @@
 class OrderPurchaser
   include ActiveModel::Model
-  attr_accessor :use_id,:item_id,:post_code,:prefecture_id,:city,:house_number,:building,:phone_number
+  attr_accessor :user_id,:item_id,:post_code,:prefecture_id,:city,:house_number,:building,:phone_number
 
   with_options presence: true do
     validates :user_id
@@ -11,10 +11,10 @@ class OrderPurchaser
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
   end
   validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Input only number"}
-  validates :prefecture, numericality: {other_than: 1, message: "can't be blank"}
+  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Purchaser.create(post_code: post_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building: building, order_id: order.id)
+    Purchaser.create(post_code: post_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building: building, phone_number: phone_number, order_id: order.id)
   end
 end
